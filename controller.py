fixed display image scaling and corrected top down

controller.py
@@ -53,7 +53,7 @@ class Controller:
 
     def set_display_image(self, image):
         self.display_image = np.copy(image)
-        self.scale_display_image()
+        self.scale_display_image(image)
         self.show_image(self.display_image)
 
     def set_image_file(self, file):
@@ -66,8 +66,8 @@ class Controller:
         self.view.btn_load_calib["state"] = "normal"
         self.messagebox.show(f"File: {self.model.image_path} loaded.")
 
-    def scale_display_image(self):
-        height, width = self.model.get_image_size(self.model.image)
+    def scale_display_image(self, image):
+        height, width = self.model.get_image_size(image)
         self.messagebox.show(f"display image width: {width}")
         self.messagebox.show(f"display image height: {height}")
 
@@ -126,7 +126,7 @@ class Controller:
             
             rescaled_points = self.rescale_picked_points(self.gathered_points)
             self.model.srcPoints = rescaled_points
-            self.messagebox.show(f"Source points: {self.model.srcPoints}")
+            self.messagebox.show(f"Source points: \n {self.model.srcPoints}")
 
     def rescale_picked_points(self, point_list):
         rescaled_list = []

model.py
@@ -51,9 +51,9 @@ class Calibration:
         #self.image = cv2.resize(_image, self.view_resolution, interpolation=cv2.INTER_CUBIC)
         
     def contour_detection(self):
-        canny = cv2.Canny(self.top_down_image, 255, 0)
+        canny = cv2.Canny(self.top_down_image, 100, 200)
         self.detected_contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        contours_draw = cv2.drawContours(self.top_down_image, self.detected_contours, -1, (0, 255, 0), 4)
+        contours_draw = cv2.drawContours(self.top_down_image, self.detected_contours, -1, (0, 255, 0), 2)
     
     def dxf_generate(self):
         dwg = ezdxf.new("R2000")
@@ -82,10 +82,10 @@ class Calibration:
         v = float(vert)
         
         aspect_ratio_of_measurement = h/v
-        img_w, _ = self.get_image_size(self.image)
+        _, img_h = self.get_image_size(self.image)
         
-        self.dest_x = int(img_w)
-        self.dest_y = int(img_w / aspect_ratio_of_measurement)
+        self.dest_x = int(img_h)
+        self.dest_y = int(img_h / aspect_ratio_of_measurement)
         
         self.dstPoints = np.float32([(self.dest_x, 0),
                                      (0, 0),
@@ -104,7 +104,7 @@ class Calibration:
 
     def topDown(self):
         #w, h = self.image.shape[:2]
-        unwarped_image = cv2.warpPerspective(self.image, self.perspectiveTransformMatrix, (self.dest_y, self.dest_x), flags=cv2.INTER_LINEAR)
+        unwarped_image = cv2.warpPerspective(self.image, self.perspectiveTransformMatrix, (self.dest_x, self.dest_y), flags=cv2.INTER_LINEAR)
 
         if self.testing:
             f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))